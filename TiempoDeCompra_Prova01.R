c97563c1eae70cfaa05cfba80298c791140ddb6b
rogercristobal5 <rogercristobalt@iqs.edu>
2020-06-03 13:58:27 +0200

    Prueba OK

--- TiempoDeCompra_Prova01.R
+++ TiempoDeCompra_Prova01.R
@@ -1,1 +1,14 @@
 ### Tiempo de Compra ###
+#### Carga de librerias y paquetes ####
+install.packages("shiny")
+install.packages("dplyr")
+if(!require("tidyverse")) {
+  install.packages("tidyverse", repos="https://cloud.r-project.org/",
+                   quiet=TRUE, type="binary")
+  library("tidyverse")
+}
+install.packages("chron")
+
+library(shiny)
+library(dplyr)
+library(chron)
